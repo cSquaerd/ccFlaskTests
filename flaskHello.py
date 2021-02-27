f8727eac0d619090a41632e69be3eaa100e5408e
Charles Patterson Cook <cook.char.p@gmail.com>
2021-02-27 14:00:35 -0500

    Added test for downloading non-public files based on session data and username

--- flaskHello.py
+++ flaskHello.py
@@ -1,4 +1,5 @@
 import flask as fl
+import os
 
 def foo():
 	return "<h1>Hello World!</h1>"
@@ -45,19 +46,44 @@ def sessionTest():
 		return "<h1>Can I see your passport?</h1>\n" + \
 			"<h2>You're not supposed to be in here!</h2>"
 
+def download(user, file):
+	if "user" in fl.session:
+		if fl.session["user"] == user:
+			if os.path.exists("./downloads/" + user + "/" + file):
+				return fl.send_from_directory("downloads/" + user + '/', file)
+			else:
+				return fl.redirect(fl.url_for("nofile"))
+		else:
+			return fl.redirect(fl.url_for("outofbounds"))
+	else:
+		return fl.redirect(fl.url_for("login"))
+
+def noFile():
+	return "<h3>The file you requested does not exist.</h3>"
+
+def outOfBounds():
+	return "<h1>You are not logged in as this user, hence you cannot see their files.</h1>"
+
 base = fl.Flask(__name__)
 base.secret_key = b"\x13\xbf\xd0r\xba\xe2<\x0b\xc0{\xa2Tn\x18T\x8c"
 base.add_url_rule('/', "index", foo)
 base.add_url_rule("/hello/<name>/", "name", bar)
+
 base.add_url_rule("/brython/", "brython", brython)
 base.add_url_rule("/dice/", "dice", dice)
+
 base.add_url_rule(
 	"/ajax/", "ajaxTest", lambda : fl.render_template("ajaxTest.html")
 )
 base.add_url_rule("/ajax/post", "ajaxPost", ajaxPost, methods = ["POST"])
+
 base.add_url_rule("/login/", "login", login, methods = ["GET", "POST"])
 base.add_url_rule("/logout/", "logout", logout)
 base.add_url_rule("/user/", "userpage", sessionTest)
+
+base.add_url_rule("/download/<user>/<file>", "download", download)
+base.add_url_rule("/error/nofile/", "nofile", noFile)
+base.add_url_rule("/error/outofbounds/", "outofbounds", outOfBounds)
 
 if __name__ == "__main__":
 	base.run(debug = True)
